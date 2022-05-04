782f5be38353495ce7f0858fa95db4a7aa2436fa
Barry Hanna <barryhanna@users.noreply.github.com>
2022-05-04 13:01:44 +0100

    Feature detection

--- examples/permissions/index.js
+++ examples/permissions/index.js
@@ -1,9 +1,16 @@
 const get_permissions = document.getElementById('get-permissions');
 const perm_status = document.getElementById('status');
-get_permissions.addEventListener('click', async () => {
-  const permission_results = await navigator.permissions.query({
-    name: 'geolocation',
+
+// Feature detection
+if ('permissions' in navigator) {
+  get_permissions.disabled = false;
+  get_permissions.addEventListener('click', async () => {
+    const permission_results = await navigator.permissions.query({
+      name: 'clipboard-read',
+    });
+    console.log('permission_results', permission_results);
+    perm_status.innerText = permission_results.state;
   });
-  console.log('permission_results', permission_results);
-  perm_status.innerText = permission_results.state;
-});
+} else {
+  perm_status.innerText = 'Your browser does not support the permissions API';
+}
