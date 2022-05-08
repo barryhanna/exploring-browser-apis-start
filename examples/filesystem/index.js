d0cefc6d14b0b66ccd339129131e646bab0cd56f
Barry Hanna <barryhanna@users.noreply.github.com>
2022-05-08 09:42:43 +0100

    Filesystem API

---
+++ examples/filesystem/index.js
@@ -0,0 +1,51 @@
+const open_button = document.getElementById('open');
+const container = document.getElementById('img-container');
+const save_button = document.getElementById('save');
+const editor = document.getElementById('editor');
+
+let file_handle;
+
+async function getFile() {
+  try {
+    let response = await window.showOpenFilePicker({
+      types: [
+        {
+          description: 'Svg Images',
+          accept: {
+            'image/svg': ['.svg'],
+          },
+        },
+      ],
+      excludeAcceptAllOption: true,
+      multiple: false,
+    });
+    console.log('response', response);
+    file_handle = response[0];
+    if (file_handle.kind === 'file') {
+      const file = await file_handle.getFile();
+      console.log('file', file);
+      const contents = await file.text();
+      console.log('contents', contents);
+      container.innerHTML = contents;
+      editor.value = contents;
+      editor.disabled = false;
+      save_button.disabled = false;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function saveFile() {
+  const writable = await file_handle.createWritable();
+  await writable.write(editor.value);
+  await writable.close();
+  alert('Saved!');
+}
+
+open_button.addEventListener('click', getFile);
+save_button.addEventListener('click', saveFile);
+
+editor.addEventListener('change', () => {
+  container.innerHTML = editor.value;
+});
